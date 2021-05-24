4db29743a0507523a9a2dad8907addcebeada168
vincehientran <vincehientran@gmail.com>
2021-05-23 22:13:38 -0500

    add road alignment and crosswalk stopping

--- MainRunner.py
+++ MainRunner.py
@@ -4,14 +4,14 @@ import numpy as np
 
 def edgeDetection(image):
     grayscale = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    blur = cv.GaussianBlur(grayscale, (3, 3), 0)
-    cannyEdge = cv.Canny(blur, 50, 150)
-    cannyEdge = cv.dilate(cannyEdge, cv.getStructuringElement(cv.MORPH_RECT, (3, 3)), iterations=2)
+    blur = cv.GaussianBlur(grayscale, (3, 3), 1)
+    cannyEdge = cv.Canny(blur, 20, 50)
+    cannyEdge = cv.dilate(cannyEdge, cv.getStructuringElement(cv.MORPH_RECT, (3, 3)), iterations=1)
     return cannyEdge
 
 def regionOfInterest(image):
     height = image.shape[0]
-    polygons = np.array([ [(0, height), (640, height), (640, height - 50), (320, 250), (0, height - 50)] ])
+    polygons = np.array([ [(10, height), (630, height), (630, height - 100), (380, 100), (260, 100), (10, height - 100)] ])
     mask = np.zeros_like(image)
     cv.fillPoly(mask, polygons, 255)
     cropped = cv.bitwise_and(image, mask)
@@ -72,6 +72,48 @@ def avgLines(image, lines):
     else:
         return []
 
+def removeHorizontal(image):
+    # initalize masks so we can remove the horizontal lines from the image
+    horizontal = np.copy(image)
+    horizontal = cv.blur(horizontal,(2,2))
+
+    # make a mask for the horizontal lines
+    cols = horizontal.shape[1]
+    horizontal_size = cols // 10
+    horizontalStructure = cv.getStructuringElement(cv.MORPH_RECT, (horizontal_size, 1))
+    horizontal = cv.erode(horizontal, horizontalStructure)
+    horizontal = cv.dilate(horizontal, horizontalStructure)
+    horizontal = cv.blur(horizontal,(6,6))
+    horizontal = (255-horizontal)
+    horizontal = cv.threshold(horizontal,250,255,cv.THRESH_BINARY)[1]
+
+    return cv.bitwise_and(image, horizontal), horizontal
+
+# aligns the vehicle to the road
+def align(lanes):
+    if lanes is not None:
+        if len(lanes) == 2:
+            slopeLeft = slope(lanes[0])
+            slopeRight = slope(lanes[1])
+            '''if (slopeLeft + slopeRight) > 0.5:
+                print('Turn Right')
+            elif (slopeLeft + slopeRight) < -0.5:
+                print('Turn Left')
+            else:
+                print('Go Straight')'''
+
+            print(slopeLeft + slopeRight)
+
+# stop the vehicle at a crosswalk
+def stop(crosswalk):
+    height = crosswalk.shape[0]
+    width = crosswalk.shape[1]
+    for i in range(height - 130, height):
+        for j in range(int(width//2) - 3, int(width//2) + 3):
+            if crosswalk[i][j] == 0:
+                return True
+    return False
+
 def slope(line):
     x1, y1, x2, y2 = line
     if (x2 - x1) == 0:
@@ -82,7 +124,6 @@ def slope(line):
 vid = cv.VideoCapture(0)
 
 while(True):
-
     # Capture the video frame
     # by frame
     ret, frame = vid.read()
@@ -90,21 +131,18 @@ while(True):
     # Display the resulting frame
     cannyEdge = edgeDetection(frame)
     mask = regionOfInterest(cannyEdge)
-    lineImage, lanes = lineDetection(mask)
-    cv.imshow('line', lineImage)
-    cv.imshow('edge', cannyEdge)
-
-    if lanes is not None:
-        if len(lanes) == 2:
-            slopeLeft = slope(lanes[0])
-            slopeRight = slope(lanes[1])
-            if (slopeLeft + slopeRight) > 0.4:
-                print('Left')
-            elif (slopeLeft + slopeRight) < -0.4:
-                print('Right')
-            else:
-                print('Straight')
-
+    mask, crosswalk = removeHorizontal(mask)
+    try:
+        lineImage, lanes = lineDetection(mask)
+        if (stop(crosswalk)):
+            print('STOP')
+        else:
+            align(lanes)
+        cv.imshow('line', lineImage)
+        cv.imshow('edge', cannyEdge)
+        cv.imshow('crosswalk', crosswalk)
+    except:
+        continue
 
     # the 'q' button is set as the
     # quitting button you may use any
